20f23b8cc83ded3f2dcbc920256a6d9627617d4e
Nilson Chaves <djnecco@gmail.com>
2025-09-08 08:19:00 -0300

    make build

---
+++ layout_calendar.707281e4.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{}),document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".calendar__day").forEach((e,t)=>{e.textContent=t+1,e.addEventListener("click",()=>{e.classList.toggle("calendar__day--active")})})});
+//# sourceMappingURL=layout_calendar.707281e4.js.map
